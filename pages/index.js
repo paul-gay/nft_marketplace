9e66d46dd712a40db6598e44a28b5671e572f755
Paul Gay <paulgay@Pauls-MacBook-Air.local>
2022-06-27 13:23:30 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// import Head from "next/head";
+// import Image from "next/image";
+
+const Home = () => (
+  <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello World
+    </h1>
+  </div>
+);
+
+export default Home;
+
